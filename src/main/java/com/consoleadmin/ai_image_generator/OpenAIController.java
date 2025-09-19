47410832c536e5e44c415d98007fc028e02ffa88
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-20 04:28:24 +0530

    changed entity response with status code

--- src/main/java/com/consoleadmin/ai_image_generator/OpenAIController.java
+++ src/main/java/com/consoleadmin/ai_image_generator/OpenAIController.java
@@ -2,6 +2,7 @@ package com.consoleadmin.ai_image_generator;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,13 @@ public class OpenAIController {
     }
 
     @GetMapping("/api/{message}")
-    public String getAnswer(@PathVariable String message) {
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
         String response = chatClient
                 .prompt(message)
                 .call()
                 .content(); // just content, other for metadata
 
-        return "Server Running with Chat Client : " + response;
+        return ResponseEntity.ok(response);
     }
 
 }
