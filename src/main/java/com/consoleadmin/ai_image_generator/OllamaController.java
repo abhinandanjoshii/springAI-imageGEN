af6a758a579ed95ec6e17900e2a4100a112f6b5b
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-20 11:23:43 +0530

    removed autoconfigure for Ollama model : local hosting model

--- src/main/java/com/consoleadmin/ai_image_generator/OllamaController.java
+++ src/main/java/com/consoleadmin/ai_image_generator/OllamaController.java
@@ -1,31 +1,40 @@
 package com.consoleadmin.ai_image_generator;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class OpenAIController {
-
+public class OllamaController {
     private ChatClient chatClient;
     private final ChatMemory chatMemory; // for storing older chats
 
-
-    public OpenAIController(OpenAiChatModel chatModel) {
+    public OllamaController(OllamaChatModel chatModel){
         this.chatMemory = MessageWindowChatMemory
                 .builder()
                 .build();
         this.chatClient = ChatClient.create(chatModel);
     }
 
-//    @GetMapping("/api/{message}")
+    // TODO : Remove in case multiple models
+//    public OllamaController(ChatClient.Builder chatClientBuilder) {
+//        this.chatMemory = MessageWindowChatMemory
+//                .builder()
+//                .build();
+//        this.chatClient = chatClientBuilder
+//                .defaultAdvisors(MessageChatMemoryAdvisor
+//                        .builder(chatMemory)
+//                        .build())
+//                .build();
+//    }
+
+    @GetMapping("/api/{message}")
     public ResponseEntity<String> getAnswer(@PathVariable String message) {
         // entire packet
         ChatResponse chatResponse = chatClient
