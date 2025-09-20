4e4c679760f5fdb5a6fd694bfddc90017a356c44
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-20 20:19:48 +0530

    pg vector store : config

--- src/main/java/com/consoleadmin/ai_image_generator/AiImageGeneratorApplication.java
+++ src/main/java/com/consoleadmin/ai_image_generator/AiImageGeneratorApplication.java
@@ -1,9 +1,10 @@
 package com.consoleadmin.ai_image_generator;
 
+import org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { PgVectorStoreAutoConfiguration.class })
 public class AiImageGeneratorApplication {
 
 	public static void main(String[] args) {
