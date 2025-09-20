d9e506d901059f0c8519f0e9899881c3dc2aa548
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-20 21:44:56 +0530

    migration -> pg vector to RedisVectorStore (running on docker)

--- src/main/java/com/consoleadmin/ai_image_generator/AiImageGeneratorApplication.java
+++ src/main/java/com/consoleadmin/ai_image_generator/AiImageGeneratorApplication.java
@@ -1,10 +1,9 @@
 package com.consoleadmin.ai_image_generator;
 
-import org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = { PgVectorStoreAutoConfiguration.class })
+@SpringBootApplication
 public class AiImageGeneratorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/consoleadmin/ai_image_generator/AppConfig.java
+++ src/main/java/com/consoleadmin/ai_image_generator/AppConfig.java
@@ -3,24 +3,25 @@ package com.consoleadmin.ai_image_generator;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import static org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgDistanceType.COSINE_DISTANCE;
-import static org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgIndexType.HNSW;
+import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public VectorStore vectorStore(JdbcTemplate jdbcTemplate,@Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
-        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
-                .dimensions(1536)
-                .distanceType(COSINE_DISTANCE)
-                .indexType(HNSW)
+    public JedisPooled jedisPooled() {
+        return new JedisPooled("localhost", 6379);
+    }
+
+    @Bean
+    public VectorStore vectorStore(JedisPooled jedisPooled , @Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
+        return RedisVectorStore.builder(jedisPooled , embeddingModel)
+                .indexName("product-index")
+                .initializeSchema(true)
                 .build();
     }
 
